e8f3f2779ca3b514932b24651901cf4266e08b19
Edd <33794654+giteed@users.noreply.github.com>
2024-10-15 21:40:54 +0300

    Update install-update.sh

--- install-update.sh
+++ install-update.sh
@@ -18,7 +18,7 @@ if ! command -v git &> /dev/null; then
     exit
 fi
 
-# Создаем папку с проектом, если она не существует, и клонируем репозиторий
+# Создаем папку проекта и клонируем репозиторий, если она не существует
 if [ ! -d "$PROJECT_DIR" ]; then
     echo "Клонирование репозитория $REPO_URL в папку $PROJECT_DIR..." | tee -a "$LOG_FILE"
     git clone "$REPO_URL" "$PROJECT_DIR" || { echo "Ошибка клонирования репозитория." | tee -a "$LOG_FILE"; exit 1; }
@@ -33,14 +33,20 @@ else
     git reset --hard HEAD || { echo "Ошибка при сбросе изменений." | tee -a "$LOG_FILE"; exit 1; }
     git pull origin main || { echo "Ошибка при обновлении репозитория." | tee -a "$LOG_FILE"; exit 1; }
 
-    # Проверка наличия start.sh и установка прав
-    if [ -f "start.sh" ]; then
-        chmod +x "start.sh" || { echo "Ошибка при установке прав на выполнение для start.sh после обновления." | tee -a "$LOG_FILE"; exit 1; }
-    else
-        echo "Файл start.sh не найден после обновления." | tee -a "$LOG_FILE"
-        exit 1
+    # Восстанавливаем start.sh, если он был удален
+    if [ ! -f "start.sh" ];then
+        echo "Файл start.sh был удалён локально, восстанавливаю его из репозитория..." | tee -a "$LOG_FILE"
+        git checkout origin/main -- start.sh || { echo "Ошибка при восстановлении start.sh." | tee -a "$LOG_FILE"; exit 1; }
     fi
+
+    # Устанавливаем права на выполнение для start.sh
+    chmod +x "start.sh" || { echo "Ошибка при установке прав на выполнение для start.sh после обновления." | tee -a "$LOG_FILE"; exit 1; }
     cd ..
+fi
+
+# Создаем папку для логов, если она не существует
+if [ ! -d "$LOG_DIR" ]; then
+    mkdir -p "$LOG_DIR" || { echo "Ошибка создания директории для логов." | exit 1; }
 fi
 
 # Перемещаемся в директорию проекта для установки зависимостей
@@ -73,4 +79,3 @@ cd ..
 
 # Завершение работы скрипта
 echo "Скрипт завершил работу. Лог записан в $LOG_FILE."
-
