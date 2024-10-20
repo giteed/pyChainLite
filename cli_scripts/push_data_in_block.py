4bd328d2d19e8f4afeced615666b21193525845e
Edd <33794654+giteed@users.noreply.github.com>
2024-10-20 18:19:25 +0300

    Update push_data_in_block.py

--- cli_scripts/push_data_in_block.py
+++ cli_scripts/push_data_in_block.py
@@ -5,14 +5,12 @@ import argparse
 import os
 import json
 import sys
-import hashlib  # Для хеширования имен блокчейнов
-from datetime import datetime
-
-# Добавляем путь к модулям в системный путь
-sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/..")
-
+import hashlib
 from modules.blockchain_loading import load_blockchain
 from modules.block_creation import create_new_block
+
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/..")
 
 BLOCKCHAIN_DIR = "blockchains"  # Путь к папке с блокчейнами
 
@@ -31,31 +29,24 @@ def push_data_to_block(blockchain_name, user_id, data):
     with open(blockchain_path, 'r') as f:
         blockchain_data = json.load(f)
 
-    # Проверяем, есть ли у пользователя права на запись в блокчейн (простая проверка для примера)
+    # Проверяем, есть ли у пользователя права на запись в блокчейн
     if user_id != blockchain_data["blocks"][0]["data"]["owner"]:
         print(f"Ошибка: Пользователь '{user_id}' не имеет прав на запись в этот блокчейн.")
         return
 
-    # Получаем номер последнего блока для определения номера нового блока
-    last_block = blockchain_data["blocks"][-1]
-    previous_hash = last_block["hash"]
-    new_block_index = last_block["index"] + 1
-
-    # Добавляем новый блок с переданными данными
-    create_new_block(blockchain_data, data)
+    # Добавляем новый блок с переданными данными и идентификатором пользователя
+    new_block = create_new_block(blockchain_data, data, user_id=user_id)
 
     # Сохраняем обновленный блокчейн
     with open(blockchain_path, 'w') as f:
         json.dump(blockchain_data, f, indent=4)
     
-    # Формируем данные для вывода
-    new_block_hash = blockchain_data["blocks"][-1]["hash"]
-    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
-    print(f"Новый блок ({new_block_index}) с данными успешно добавлен в блокчейн '{blockchain_name}'.")
-    print(f"Время добавления: {timestamp}")
-    print(f"Хэш нового блока: {new_block_hash}")
-    print(f"Хэш предыдущего блока: {previous_hash}")
+    # Выводим информацию о новом блоке
+    print(f"Новый блок ({new_block.index}) с данными успешно добавлен в блокчейн '{blockchain_name}'.")
+    print(f"Время добавления: {new_block.timestamp}")
+    print(f"Хэш нового блока: {new_block.hash}")
+    print(f"Хэш предыдущего блока: {new_block.previous_hash}")
+    print(f"Добавлен пользователем: {new_block.data['added_by']}")
 
 if __name__ == "__main__":
     # Парсинг аргументов командной строки
