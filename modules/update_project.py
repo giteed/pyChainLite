50df99198e813ece7bd38d02ca948dda4fbc9638
Edd <33794654+giteed@users.noreply.github.com>
2024-10-20 11:45:45 +0300

    Update update_project.py

--- modules/update_project.py
+++ modules/update_project.py
@@ -1,5 +1,7 @@
 # modules/update_project.py
 # Модуль для обновления проекта
+
+import os
 import subprocess
 import shutil
 from rich.console import Console
@@ -9,5 +11,28 @@ console = Console()
 def update_project():
     console.print("🔄 [bold cyan]Запуск обновления проекта...[/bold cyan]")
     subprocess.run(['chmod', '+x', './install-update.sh'], check=True)
+    
+    # Копируем файл в родительский каталог
     shutil.copy('./install-update.sh', '../install-update.sh')
-    subprocess.run(['../install-update.sh'], check=True)
+    
+    # Переходим в родительский каталог
+    os.chdir('..')
+
+    # Запускаем перемещенный файл
+    subprocess.run(['./install-update.sh'], check=True)
+
+    # Предлагаем перезапустить меню после обновления
+    console.print("[bold green]Проект обновлен. Перезапуск меню...[/bold green]")
+
+    # Перезапускаем меню
+    restart_menu()
+
+def restart_menu():
+    """
+    Перезапуск скрипта menu.py
+    """
+    # Получаем путь к текущему скрипту
+    script_path = os.path.join(os.getcwd(), 'pyChainLite', 'menu.py')
+
+    # Запускаем новый процесс с menu.py и выходим из текущего
+    os.execv(script_path, ['python3', script_path])
