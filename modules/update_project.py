f62140e3cd2da0a0f4c173170652b918352e13d5
Edd <33794654+giteed@users.noreply.github.com>
2024-10-20 11:50:19 +0300

    Update update_project.py

--- modules/update_project.py
+++ modules/update_project.py
@@ -1,30 +1,37 @@
 # modules/update_project.py
-# Модуль для обновления проекта
+# Модуль для обновления проекта и перезапуска меню
 
 import os
 import subprocess
 import shutil
-from rich.console import Console
 
-console = Console()
+BLOCKCHAIN_DIR = "blockchains"
 
 def update_project():
-    console.print("🔄 [bold cyan]Запуск обновления проекта...[/bold cyan]")
+    """
+    Функция для обновления проекта.
+    """
+    print("🔄 [bold cyan]Запуск обновления проекта...[/bold cyan]")
+    
+    # Устанавливаем права на выполнение для скрипта установки и обновления
     subprocess.run(['chmod', '+x', './install-update.sh'], check=True)
     
-    # Копируем файл в родительский каталог
-    shutil.copy('./install-update.sh', '../install-update.sh')
-    
-    # Переходим в родительский каталог
-    os.chdir('..')
+    # Перемещаем папку блокчейнов в родительский каталог для сохранности
+    if os.path.exists(BLOCKCHAIN_DIR):
+        parent_dir = os.path.abspath(os.path.join(os.getcwd(), ".."))
+        backup_blockchain_dir = os.path.join(parent_dir, BLOCKCHAIN_DIR)
+        shutil.move(BLOCKCHAIN_DIR, backup_blockchain_dir)
+        print(f"[2024-10-20] Папка с блокчейнами временно перемещена в {backup_blockchain_dir}.")
 
-    # Запускаем перемещенный файл
+    # Запускаем скрипт обновления
     subprocess.run(['./install-update.sh'], check=True)
 
-    # Предлагаем перезапустить меню после обновления
-    console.print("[bold green]Проект обновлен. Перезапуск меню...[/bold green]")
+    # Возвращаем папку с блокчейнами обратно в проект
+    if os.path.exists(backup_blockchain_dir):
+        shutil.move(backup_blockchain_dir, os.getcwd())
+        print(f"[2024-10-20] Папка с блокчейнами успешно возвращена в проект.")
 
-    # Перезапускаем меню
+    # Перезапуск меню
     restart_menu()
 
 def restart_menu():
@@ -32,7 +39,10 @@ def restart_menu():
     Перезапуск скрипта menu.py
     """
     # Получаем путь к текущему скрипту
-    script_path = os.path.join(os.getcwd(), 'pyChainLite', 'menu.py')
+    script_path = os.path.join(os.getcwd(), 'menu.py')
+
+    # Устанавливаем права на выполнение для скрипта menu.py
+    subprocess.run(['chmod', '+x', script_path], check=True)
 
     # Запускаем новый процесс с menu.py и выходим из текущего
     os.execv(script_path, ['python3', script_path])
