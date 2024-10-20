233db233a63306ce1608cf0b7954762d13c92a10
Edd <33794654+giteed@users.noreply.github.com>
2024-10-20 07:48:57 +0300

    Update update_project.py

--- modules/update_project.py
+++ modules/update_project.py
@@ -1,2 +1,13 @@
-# update_project.py
+# modules/update_project.py
 # Модуль для обновления проекта
+import subprocess
+import shutil
+from rich.console import Console
+
+console = Console()
+
+def update_project():
+    console.print("🔄 [bold cyan]Запуск обновления проекта...[/bold cyan]")
+    subprocess.run(['chmod', '+x', './install-update.sh'], check=True)
+    shutil.copy('./install-update.sh', '../install-update.sh')
+    subprocess.run(['../install-update.sh'], check=True)
