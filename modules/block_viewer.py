224da5a262fd18630c578298b2d867391e0f28c3
Edd <33794654+giteed@users.noreply.github.com>
2024-10-20 07:47:48 +0300

    Update block_viewer.py

--- modules/block_viewer.py
+++ modules/block_viewer.py
@@ -1,2 +1,12 @@
-# block_viewer.py
+# modules/block_viewer.py
 # Модуль для просмотра блоков
+import json
+from rich.console import Console
+
+console = Console()
+
+def view_blocks(current_blockchain):
+    if current_blockchain:
+        console.print(json.dumps(current_blockchain, indent=4))
+    else:
+        console.print("[red]Сначала загрузите блокчейн.[/red]")
