cf8bb10c1e2ef369855964877e1dbaab2fb7e241
Edd <33794654+giteed@users.noreply.github.com>
2024-10-20 18:15:30 +0300

    Update blockchain_creation.py

--- modules/blockchain_creation.py
+++ modules/blockchain_creation.py
@@ -1,36 +1,40 @@
-# modules/blockchain_creation.py
-# Модуль для создания блокчейна
+# modules/block_creation.py
+# Модуль для создания блоков и просмотра блоков в текущем блокчейне
+
 import os
 import json
-import hashlib
-from src.blockchain import Block
 from rich.console import Console
+from src.blockchain import Block
+from datetime import datetime
 
 console = Console()
+
 BLOCKCHAIN_DIR = "blockchains"
 
-def hash_password(password):
-    return hashlib.sha256(password.encode()).hexdigest()
+def create_new_block(current_blockchain, data, user_id=None):
+    """
+    Создание нового блока в загруженном блокчейне.
+    """
+    last_block = current_blockchain["blocks"][-1]
+    new_block = Block(
+        index=last_block["index"] + 1,
+        data={
+            "data": data,
+            "added_by": user_id,  # Пометка, кто добавил блок
+            "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S")  # Время добавления блока
+        },
+        previous_hash=last_block["hash"]
+    )
 
-def create_blockchain():
-    blockchain_name = input("Введите имя для нового блокчейна: ").strip()
-    blockchain_owner = input("Введите имя владельца блокчейна: ").strip()
-    owner_password = input("Введите пароль для владельца блокчейна: ").strip()
+    current_blockchain["blocks"].append(new_block.__dict__)
 
-    hashed_password = hash_password(owner_password)
-    blockchain_hash = hashlib.sha256(blockchain_name.encode()).hexdigest()
-    blockchain_file = f"{blockchain_hash}.json"
+    blockchain_file = current_blockchain["file"]
     blockchain_path = os.path.join(BLOCKCHAIN_DIR, blockchain_file)
-
-    if os.path.exists(blockchain_path):
-        console.print(f"[red]Блокчейн с именем '{blockchain_name}' уже существует.[/red]")
-        return
-
-    os.makedirs(BLOCKCHAIN_DIR, exist_ok=True)
-    genesis_block = Block(0, {"blockchain_name": blockchain_name, "owner": blockchain_owner, "owner_password_hash": hashed_password}, "0" * 64)
-    blockchain_data = {"blocks": [genesis_block.__dict__]}
-
+    
+    # Сохраняем новый блок в файл блокчейна
     with open(blockchain_path, 'w') as f:
-        json.dump(blockchain_data, f, indent=4)
-
-    console.print(f"\nБлокчейн '{blockchain_name}' успешно создан.")
+        json.dump(current_blockchain, f, indent=4)
+    
+    console.print(f"[green]Новый блок успешно добавлен в блокчейн {current_blockchain['blocks'][0]['data']['blockchain_name']}.[/green]")
+    
+    return new_block  # Возвращаем созданный блок для дальнейшего использования
