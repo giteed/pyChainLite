c4391ce6a59ad45a0aef47167ed665418e121c32
Edd <33794654+giteed@users.noreply.github.com>
2024-10-22 00:15:46 +0300

    Update push_data_in_block.py

--- cli_scripts/push_data_in_block.py
+++ cli_scripts/push_data_in_block.py
@@ -1,5 +1,7 @@
 # cli_scripts/push_data_in_block.py
 # Скрипт для добавления данных в блок блокчейна через аргументы командной строки
+# Пример использования:
+# python3 push_data_in_block.py --blockchain-name alfa --uid edd "cli test add data to block"
 
 import argparse
 import os
@@ -15,9 +17,7 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/..")
 from modules.blockchain_loading import load_blockchain
 from modules.block_creation import create_new_block
 
-# Определяем абсолютный путь к папке blockchains
-PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-BLOCKCHAIN_DIR = os.path.join(PROJECT_ROOT, "blockchains")
+BLOCKCHAIN_DIR = "blockchains"  # Путь к папке с блокчейнами
 
 console = Console()
 
@@ -39,7 +39,7 @@ def push_data_to_block(blockchain_name, user_id, data):
 
     blockchain_file = f"{blockchain_hash}.json"
     blockchain_path = os.path.join(BLOCKCHAIN_DIR, blockchain_file)
-    console.print(f"[blue]Отладка:[/blue] Абсолютный путь к файлу блокчейна: {blockchain_path}")
+    console.print(f"[blue]Отладка:[/blue] Путь к файлу блокчейна: {blockchain_path}")
 
     # Проверяем, существует ли блокчейн
     if not os.path.exists(blockchain_path):
@@ -65,18 +65,18 @@ def push_data_to_block(blockchain_name, user_id, data):
     new_block = create_new_block(blockchain_data, data, user_id=user_id)
 
     # Преобразуем timestamp в читаемый формат
-    readable_time = datetime.fromtimestamp(float(new_block["timestamp"])).strftime('%Y-%m-%d %H:%M:%S')
+    readable_time = datetime.fromtimestamp(float(new_block.timestamp)).strftime('%Y-%m-%d %H:%M:%S')
 
     # Сохраняем обновленный блокчейн
     with open(blockchain_path, 'w') as f:
         json.dump(blockchain_data, f, indent=4)
     
     # Выводим информацию о новом блоке
-    console.print(f"[green]Новый блок ({new_block['index']}) с данными успешно добавлен в блокчейн '{blockchain_name}'.[/green]")
+    console.print(f"[green]Новый блок ({new_block.index}) с данными успешно добавлен в блокчейн '{blockchain_name}'.[/green]")
     console.print(f"[blue]Время добавления:[/blue] {readable_time}")
-    console.print(f"[blue]Хэш нового блока:[/blue] {new_block['hash']}")
-    console.print(f"[blue]Хэш предыдущего блока:[/blue] {new_block['previous_hash']}")
-    console.print(f"[blue]Добавлен пользователем:[/blue] {new_block['data'].get('added_by')}")
+    console.print(f"[blue]Хэш нового блока:[/blue] {new_block.hash}")
+    console.print(f"[blue]Хэш предыдущего блока:[/blue] {new_block.previous_hash}")
+    console.print(f"[blue]Добавлен пользователем:[/blue] {new_block.data['added_by']}")
 
 if __name__ == "__main__":
     # Парсинг аргументов командной строки
